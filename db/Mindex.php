e5f708980f5cb2e2998c6824d3d08930901da5be
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-08-14 21:49:44 -0300

    R

---
+++ db/Mindex.php
@@ -0,0 +1,13 @@
+<?php
+// $SenhaD = $_GET[senha];
+// $ContaD = $_GET[email];
+$ContasC = array(
+    array("jose.barbosa111@etec.sp.gov.br","12345678"),
+    array("cristovao.santos@etec.sp.gov.br","csms2222"),
+    array("joao.guimaraes48@etec.sp.gov.br","SalesLindo"),
+    array("joelma.ribeiro3@etec.sp.gov.br","etecab107")
+    );
+echo "a: "+$CosntasC(1,2)
+
+
+?>
