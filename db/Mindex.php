facea87a336715b9ff9841490880c2b5b9b0a46c
cristopher <cristovao.santos@etec.sp.gov.br>
2024-08-23 13:01:19 -0300

    Arrumado o php e segunda etapa de organização

--- db/Mindex.php
+++ db/Mindex.php
@@ -1,13 +1,40 @@
 <?php
-// $SenhaD = $_GET[senha];
-// $ContaD = $_GET[email];
-$ContasC = array(
-    array("jose.barbosa111@etec.sp.gov.br","12345678"),
-    array("cristovao.santos@etec.sp.gov.br","csms2222"),
-    array("joao.guimaraes48@etec.sp.gov.br","SalesLindo"),
-    array("joelma.ribeiro3@etec.sp.gov.br","etecab107")
-    );
-echo "a: "+$CosntasC(1,2)
+$SenhaD = $_GET["senha"];
+$ContaD = $_GET["email"];
+$ContasC = [
+    [
+        "jose.barbosa111@etec.sp.gov.br",
+        "12345678"
+    ],
+    [
+        "cristovao.santos@etec.sp.gov.br",
+        "csms2222"
+    ],
+    [
+        "joao.guimaraes48@etec.sp.gov.br",
+        "SalesLindo"
+    ],
+    [
+        "joelma.ribeiro3@etec.sp.gov.br",
+        "etecab107"
+    ],
+    [
+        "test",
+        "testando"
+    ]
+];
+$i = 0;
 
-
+foreach ($ContasC as $key) {
+    if($key[0] == $ContaD)
+    {
+        if ($key[1] = $SenhaD) {
+             echo "<script>window.location.replace('Menu.html');</script>";
+        }
+        else{
+        echo "<script>window.location.replace('index.html');</script>";
+        }
+    }
+}
+echo "<script>window.location.replace('index.html');</script>";
 ?>
